b5adc0405450de9ed8e2791b46979933565fbb80
n-thoma <nthoma3851@gmail.com>
2023-08-25 19:00:21 -0700

    Update spotify.py
    Added a few new features:
    - Enable Shuffle
    - Disable Shuffle
    - Play Compilation Playlists of any Verified Artists
    - Play Personal Playlists
    - Pause
    - Resume
    - Skip Next
    - Skip Previous

--- spotify.py
+++ spotify.py
@@ -4,13 +4,14 @@
 # Libraries
 import webbrowser
 import json
+import random
 
 
 # External Files
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.oauth2 import SpotifyPKCE
 from config import spotify_client_id, spotify_client_secret
-from requests import put, get
+from requests import put, get, post
 
 
 # Variables
@@ -86,6 +87,52 @@ def get_song_position(token, album_id, track_id):
     return None
 
 
+def get_artist_spotify_playlist(token, artist_name):
+    url = "https://api.spotify.com/v1/search"    # url to request.get
+    headers = get_auth_header(token)             # gets the authorization header
+    query = f"?q=This+is+{artist_name}&type=playlist"  # queries search to first result
+
+    query_url = url + query                                                 # creates the query url
+    result = get(query_url, headers=headers)                                # response.get the results
+    return json.loads(result.content)["playlists"]["items"][0]["id"]  # returns the playlist id
+
+
+def enable_shuffle(token):
+    url = "https://api.spotify.com/v1/me/player/shuffle?state=true"
+    headers = get_auth_header(token)
+    put(url, headers=headers)
+
+
+def disable_shuffle(token):
+    url = "https://api.spotify.com/v1/me/player/shuffle?state=false"
+    headers = get_auth_header(token)
+    put(url, headers=headers)
+
+
+def get_playlist_length(token, playlist_id):
+    url = f"https://api.spotify.com/v1/playlists/{playlist_id}"
+    headers = get_auth_header(token)             # gets the authorization header
+
+    result = get(url, headers=headers)                                # response.get the results
+    return json.loads(result.content)["tracks"]["total"]
+
+
+def get_playlist_id(token, playlist_name):
+    url = "https://api.spotify.com/v1/me/playlists?limit=50"
+    headers = get_auth_header(token)
+
+    result = get(url, headers=headers)
+
+    if result.status_code == 200:
+        playlists_data = result.json()
+        for playlist in playlists_data['items']:
+            if playlist['name'].lower() == playlist_name:
+                return playlist['id']
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+
 def play_song(token, song_name):
     album_id = get_song_album_id(token, song_name)          # gets the album id that song is in
     song_id = get_song_id(token, song_name)                 # gets the song id
@@ -104,9 +151,96 @@ def play_song(token, song_name):
         "position_ms": 0
     }
 
-    response = put(url, headers=headers, json=data)  # PUT request
+    result = put(url, headers=headers, json=data)  # PUT request
+
+    if result.status_code != 204:
+        return "[SYSTEM] Could not find that song."
+    else:
+        return "[SYSTEM] Song found"
+
+
+def next_song(token):
+    url = "https://api.spotify.com/v1/me/player/next"
+    headers = get_auth_header(token)
+
+    post(url, headers=headers)
+
+
+def previous_song(token):
+    url = "https://api.spotify.com/v1/me/player/next"
+    headers = get_auth_header(token)
+
+    post(url, headers=headers)
+
+
+def pause(token):
+    url = "https://api.spotify.com/v1/me/player/pause"
+    headers = get_auth_header(token)
+
+    put(url, headers=headers)
+
+
+def resume(token):
+    url = "https://api.spotify.com/v1/me/player/play"
+    headers = get_auth_header(token)
+
+    put(url, headers=headers)
+
+
+def play_artist(token, artist_name):
+    playlist_id = get_artist_spotify_playlist(token, artist_name)
+    enable_shuffle(token)
+    playlist_length = get_playlist_length(token, playlist_id) - 1
+    random_song_position = random.randint(0, playlist_length)
+
+    url = "https://api.spotify.com/v1/me/player/play"  # gets the url to PUT request to
+    headers = {
+        "Authorization": "Bearer " + token,            # creates the authorization
+        "Content-Type": "application/json"
+    }
+    data = {
+        "context_uri": f"spotify:playlist:{playlist_id}",    # looks for the album the song is in
+        "offset": {
+            "position": + random_song_position,                     # puts in the position where the song is
+        },
+        "position_ms": 0
+    }
+
+    result = put(url, headers=headers, json=data)  # PUT request
+
+    if result.status_code != 204:
+        return "[SYSTEM] Could not find that artist."
+    else:
+        return "[SYSTEM] Artist found."
+
+
+def play_playlist(token, playlist_name):
+    playlist_id = get_playlist_id(token, playlist_name)
+    enable_shuffle(token)
+    playlist_length = get_playlist_length(token, playlist_id) - 1
+    random_song_position = random.randint(0, playlist_length)
+
+    url = "https://api.spotify.com/v1/me/player/play"  # gets the url to PUT request to
+    headers = {
+        "Authorization": "Bearer " + token,            # creates the authorization
+        "Content-Type": "application/json"
+    }
+    data = {
+        "context_uri": f"spotify:playlist:{playlist_id}",    # looks for the album the song is in
+        "offset": {
+            "position": + random_song_position,             # puts in the position where the song is
+        },
+        "position_ms": 0
+    }
+
+    result = put(url, headers=headers, json=data)  # PUT request
+
+    if result.status_code != 204:
+        return "[SYSTEM] Could not find that playlist."
+    else:
+        return "[SYSTEM] Playlist found."
 
 
 # Main
-token = get_access_token()     # gets token
-play_song(token, "Moonlight")  # plays Moonlight by Kali Uchis (or sometimes XXXTentacion)
+token = get_access_token()
+print(play_playlist(token, "electronic music"))
